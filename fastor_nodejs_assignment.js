fd156027a909f35d56253dc0dc9ab994cdd619cb
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-11-03 10:19:42 +0530

    Add files via upload

---
+++ fastor_nodejs_assignment.js
@@ -0,0 +1,32 @@
+{
+  "name": "fastor-nodejs-assignment",
+  "version": "1.0.0",
+  "description": "Fastor Node.js Developer Assignment - CRM REST API using Express, Sequelize, and SQLite",
+  "main": "fastor-nodejs-assignment.js",
+  "scripts": {
+    "start": "node fastor-nodejs-assignment.js",
+    "dev": "nodemon fastor-nodejs-assignment.js"
+  },
+  "keywords": [
+    "Fastor",
+    "Node.js",
+    "Express",
+    "Sequelize",
+    "SQLite",
+    "JWT",
+    "CRM"
+  ],
+  "author": "Samatha Pulgam",
+  "license": "MIT",
+  "dependencies": {
+    "bcryptjs": "^2.4.3",
+    "body-parser": "^1.20.3",
+    "express": "^4.21.1",
+    "jsonwebtoken": "^9.0.2",
+    "sequelize": "^6.37.1",
+    "sqlite3": "^5.1.7"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.7"
+  }
+}
